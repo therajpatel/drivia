83aea30b838c35a343a761c1c724d4e9cf1caaaa
Raj Patel <105919468+therajpatel@users.noreply.github.com>
2022-05-24 07:51:57 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -172,7 +172,7 @@
           confirmButtonText: "NEXT",
   
           html : true,
-          confirmButtonColor: "rgb(50 50 122)",
+          confirmButtonColor: "rgb(245 222 210)",
           
         };
   
