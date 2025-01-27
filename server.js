a34be238e2adb153b67a8927e26c2b63c4d0e662
Raj Patel <105919468+therajpatel@users.noreply.github.com>
2025-01-26 23:21:46 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { fetchQuestionsFromAPI } = require("./api");
-const cors = require("node-cors");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const axios = require("axios");
